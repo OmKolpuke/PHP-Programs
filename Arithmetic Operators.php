5f0aa8909c53cebd234a62a75e3b2313fe6171e3
OmKolpuke <104248716+OmKolpuke@users.noreply.github.com>
2022-06-28 23:07:18 +0530

    Arithmetic Operators.php

---
+++ Arithmetic Operators.php
@@ -0,0 +1,23 @@
+<?php
+echo "<b> Arithmetic Operators <b><br>";
+$x = 10;
+$y = 6; 
+
+echo "Addition = ".$x + $y;
+echo "<br>";
+echo "substraction =".$x - $y;
+echo "<br>";
+echo "Multiplication =".$x * $y;
+echo "<br>";
+echo "Division =".$x / $y;
+echo "<br>";
+echo "Modulus =".$x % $y;
+echo "<br>";
+echo "Expotentiation =".$x ** $y;
+
+?>
+
+
+
+
+
